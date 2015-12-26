47f0a919dbd17006b001df3e38cbc93792951e66
sbstn <sbstn.jmnz@yahoo.com>
2015-12-26 14:50:12 -0300

    keys working

--- synth.js
+++ synth.js
@@ -1,1 +1,30 @@
-console.log('I am ready to rock!');
+var context = new AudioContext(),
+  volume = context.createGain(),
+  oscillators = {};
+
+  volume.gain.value = 0.5;
+
+  volume.connect(context.destination);
+
+  //osc.stop(context.currentTime + 1);
+  //osc.start(context.currentTime); 
+
+var keyboard = new QwertyHancock({
+  id: 'keyboard',
+  octaves: 2
+});
+
+keyboard.keyDown = function (note, frequency) {
+  var osc = context.createOscillator();
+
+  oscillators[note] = osc;
+
+  osc.connect(volume);
+  osc.frequency.value = frequency;
+  osc.start(context.currentTime);
+};
+
+keyboard.keyUp = function (note, frequency) {
+  oscillators[note].stop(context.currentTime);
+  oscillators[note].disconnect();
+};
